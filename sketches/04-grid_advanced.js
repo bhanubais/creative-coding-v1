e4701daff637148d8eacd8dece75d21fc05b928c
Bhanu Bais <bhanubais@gmail.com>
2025-09-29 09:22:12 +0530

    Line based basic grid introduced

---
+++ sketches/04-grid_advanced.js
@@ -0,0 +1,61 @@
+// Import the library
+const canvasSketch = require('canvas-sketch');
+
+// Specify some output parameters
+const settings = {
+  // The [width, height] of the artwork in pixels
+  dimensions: [ 1024, 1024 ]
+};
+
+// Start the sketch
+const sketch = (props) => {
+  // Destructure what we need from props
+  const { context, width, height } = props;
+
+  const gridW = width * .8;
+  const gridH = height * .8;
+  const cols = 10;
+  const rows = 10;
+  const cells = cols * rows;
+  const marginX = (width - gridW) * .5;
+  const marginY = (height - gridH) * .5;
+  const cellW = gridW / cols;
+  const cellH = gridH / rows;
+
+  return () => {
+    // Fill the canvas with pink
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    for (let i = 0; i < cells; i++) {
+      const col = i % cols;
+      const row = Math.floor(i / cols);
+
+      console.log(col, row);
+
+      const w = cellW * .8;
+      const h = cellH * .8;
+      const x = cellW * col;
+      const y = cellH * row;
+
+      context.save();
+      context.translate(marginX, marginY);
+      context.translate(x, y);
+      context.translate(cellW * .5, cellH * .5);
+
+      context.lineWidth = 4;
+
+      context.beginPath();
+      context.moveTo(-w * .5, 0);
+      context.lineTo(w * .5, 0);
+      context.stroke();
+
+      context.restore();
+
+    }
+
+  };
+};
+
+// Start the sketch with parameters
+canvasSketch(sketch, settings);
