c0a4c69547b848b1e1379739012cc711827c561e
bhanubais <bhanubais@gmail.com>
2025-11-01 14:22:39 +0530

    practice dhamaka

---
+++ practices/02-dhamaka.js
@@ -0,0 +1,26 @@
+// Import the library
+const canvasSketch = require('canvas-sketch');
+
+// Specify some output parameters
+const settings = {
+  // The [width, height] of the artwork in pixels
+  dimensions: [ 800, 800 ]
+};
+
+// Start the sketch
+const sketch = (props) => {
+  // Destructure what we need from props
+  const { context, width, height } = props;
+
+  
+
+  return () => {
+    // Fill the canvas with pink
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+  };
+};
+
+// Start the sketch with parameters
+canvasSketch(sketch, settings);
